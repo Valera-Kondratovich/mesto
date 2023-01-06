b7885d62b77f3bbb5b6068ca3f1acf80f06fddae
Valera Kondratovich <kondratovichval@yandex.ru>
2023-01-06 21:47:48 +0300

    внес исправления итерация 1

--- scripts/script.js
+++ scripts/script.js
@@ -4,22 +4,27 @@ let buttonFormClose = document.querySelector('.popup__button-close');
 let buttonFormSave = document.querySelector('.popup__button-save');
 let porfileName = document.querySelector('.profile__name');
 let profileDescr = document.querySelector('.profile__description');
-let popupUserName = document.querySelector('.popup__user-name');
-let popupUserDescr = document.querySelector('.popup__user-description');
+let popupUserName = document.querySelector('.popup__user_name');
+let popupUserDescr = document.querySelector('.popup__user_description');
+let formUser = document.querySelector('.popup__profile-edit');
 
 function formEditDisplay() {
-  formProfile.classList.toggle('popup_visible');
+  formProfile.classList.add('popup_visible');
   popupUserName.value = porfileName.textContent;
   popupUserDescr.value = profileDescr.textContent;
-
 }
 
-buttonFormEdit.addEventListener('click', formEditDisplay);
-buttonFormClose.addEventListener('click', formEditDisplay);
+function formEditClose() {
+  formProfile.classList.remove('popup_visible');
+}
 
-function formEditSave() {
+function formEditSave(evt) {
+  evt.preventDefault();
   porfileName.textContent = popupUserName.value;
   profileDescr.textContent = popupUserDescr.value;
   formProfile.classList.remove('popup_visible');
 }
-buttonFormSave.addEventListener('click', formEditSave);
+
+formUser.addEventListener('submit', formEditSave);
+buttonFormEdit.addEventListener('click', formEditDisplay);
+buttonFormClose.addEventListener('click', formEditClose);
