c518cdac48cb5de72c390e5cdd4a4ac087198753
Valera Kondratovich <kondratovichval@yandex.ru>
2023-01-24 15:23:50 +0300

    исправление замечаний итерация 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const buttonFormEdit = document.querySelector('.profile__button-edit'); //нашел кнопку редактирования профиля
-const formEdit = document.querySelector('.popup__edit'); //нашел попап окно редактирования профиля
+const formEdit = document.querySelector('.popup_edit'); //нашел попап окно редактирования профиля
 const buttonFormClose = formEdit.querySelector('.popup__button-close'); //нашел кнопку закрытия профиля
 const buttonFormSave = formEdit.querySelector('.popup__edit__button-save'); //нашел кнопку сохранения профиля
 const porfileName = document.querySelector('.profile__name'); //сохранил имя профиля
@@ -8,7 +8,7 @@ const popupUserName = formEdit.querySelector('.popup__input_name'); //нашел
 const popupUserDescr = formEdit.querySelector('.popup__input_description'); //нашел поле ввода описания профиля в попап окне
 const formUser = document.querySelector('.popup__profile'); //нашел форму профиля
 const buttonGalleryAdd = document.querySelector('.profile__button-add'); // нашел кнопку добавления картинки
-const popupGallery = document.querySelector('.popup__add'); //попап добавления картинки
+const popupGallery = document.querySelector('.popup_add'); //попап добавления картинки
 const buttonGalleryClose = popupGallery.querySelector('.popup__button-close'); // кнопка закрытия попап окна
 const buttonGallerySave = popupGallery.querySelector('.popup__button-save'); // кнопка сохранения попап окна
 const inputHeaderGallery = popupGallery.querySelector('.popup__input_header'); //поле ввода описания в попап окне
@@ -47,7 +47,7 @@ const createCard = (name, link) => {
   const buttonDel = galleryElement.querySelector('.elements__button-trash');
   const buttonImage = galleryElement.querySelector('.elements__img');
   const titleImage = galleryElement.querySelector('.elements__title');
-  const popupImage = document.querySelector('.popup__image');
+  const popupImage = document.querySelector('.popup_image');
   const inputImage = popupImage.querySelector('.popup__picture');
   const buttonCloseImage = popupImage.querySelector('.popup__button-close');
   const depiction = popupImage.querySelector('.popup__description');
