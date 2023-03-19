44f088169fc8a30c714464a1acd2e2130bd0eeeb
Valera Kondratovich <kondratovichval@yandex.ru>
2023-03-19 21:19:40 +0300

    курс 04 спринт 8 итерация 2

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,16 +1,17 @@
 export default class Popup {
   constructor(selector) {
     this._popupElement = document.querySelector(selector); //селектор попап картинки
+    this._handleEscClose = this._handleEscClose.bind(this)
   }
 
   open() {
     this._popupElement.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popupElement.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
